c70c1e1cb34ab1dae1e77b3852ba47ffb965b1e1
Humaniuc <humancoria@gmail.com>
2019-05-09 22:22:45 +0300

    Create classes

---
+++ GSMProject/GSMProject/GSM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSMProject
+{
+    class GSM
+    {
+        private string model;
+        private string manufacturer;
+        private double price;
+        private string owner;
+        private Battery battery;
+        private Display display;
+    }
+}
